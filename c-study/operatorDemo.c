39b396161a25c5002be996f40cd0a37533a0df2a
castlepan <panye819@gmail.com>
2018-01-26 14:10:58 +0800

    add operator

---
+++ c-study/operatorDemo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main11()
+{
+    int a = 5, b =2;
+    printf("a / b = %d\n" ,(a/b));
+    double result = a/b;
+    printf("a / b = %lf" ,result);
+
+}
---
+++ c-study/sizeOfDemo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("sizeof(int) = %d\n", sizeof(int));
+    printf("sizeof(long) = %d\n", sizeof(1000L));
+    printf("sizeof(float) = %d\n", sizeof(34.4f));
+    printf("sizeof(double) = %d\n", sizeof(34.4));
+    printf("sizeof(char) = %d\n", sizeof(char));
+
+    return 0;
+}
